8fe672fc8f4aede3da65f34e61e05a234acb9215
Greison Nicolau Böff <greisonboff@hotmail.com>
2023-05-29 20:29:43 -0300

    Create code-bkp.js

---
+++ code-bkp.js
@@ -0,0 +1,299 @@
+//Json innfos
+var jsonInfos = [
+    {
+        "categoria": "React",
+        "link": "https://cursos.alura.com.br/certificate/greison-boff/react-crie-primeira-pagina-web",
+        "nome_curso": "React: Comece seu projeto full stack"
+    },
+    {
+        "categoria": "React Native",
+        "link": "https://cursos.alura.com.br/certificate/greison-boff/react-native-informacoes-localmente",
+        "nome_curso": "React Native: Guardando informações localmente"
+    },
+    {
+        "categoria": "Ferramentas Google",
+        "link":"https://cursos.alura.com.br/certificate/greison-boff/google-tag-manager-aplicacao-gerenciamento-tags",
+        "nome_curso":"Google Tag Manager: aplicação e gerenciamento de Tags"
+    },
+    {
+        "categoria": "React Native",
+        "link": "https://cursos.alura.com.br/certificate/greison-boff/react-native-utilizando-web-api",
+        "nome_curso": "React Native: Utilizando web api"
+    },
+    {
+        "categoria": "React",
+        "link": "https://cursos.alura.com.br/certificate/greison-boff/react-desenvolvendo-javascript",
+        "nome_curso": "React: Desenvolvendo Javascript"
+    },
+    {
+        "categoria": "React Native",
+        "link": "https://cursos.alura.com.br/certificate/greison-boff/react-native-criando-menu-navegando-telas",
+        "nome_curso": "React Native: Criando menu e navegando entre telas"
+    },
+    {
+        "categoria": "React Native",
+        "link": "https://cursos.alura.com.br/certificate/greison-boff/react-native-utilizando-criando-hooks",
+        "nome_curso": "React Native: Utilizando e criando Hooks"
+    },
+    {
+        "categoria": "React Native",
+        "link": "https://cursos.alura.com.br/certificate/greison-boff/react-native-comecando-zero",
+        "nome_curso": "React Native: Criando um app"
+    },
+    {
+        "categoria": "Ferramentas Google",
+        "link":"https://cursos.alura.com.br/certificate/greison-boff/google-analytics-implementacao-relatorios",
+        "nome_curso":"Google Analytics 4: implementação e relatórios"
+    },
+    {
+        "categoria": "React",
+        "link": "https://cursos.alura.com.br/certificate/3a28522b-8205-4989-ae5f-9b7a0c98a442",
+        "nome_curso": "React: entendendo como a biblioteca funciona"
+    },
+    {
+        "categoria": "Ferramentas Google",
+        "link": "https://cursos.alura.com.br/certificate/caad4838-bcfe-472d-91f4-c9e3bbccfc34",
+        "nome_curso": "BigQuery: o banco de dados da Google para Big Data"
+    },
+    {
+        "categoria": "Ferramentas Google",
+        "link": "https://cursos.alura.com.br/certificate/3c2262e8-64cd-4c3e-9c10-9e33b0526d39",
+        "nome_curso": "BigQuery: funções do BigQuery"
+    },
+    {
+        "categoria": "Ferramentas Google",
+        "link": "https://cursos.alura.com.br/certificate/567690c3-3203-4ea0-9b4f-eac7a3f05aaa",
+        "nome_curso": "Google Data Studio: filtros e funções"
+    },
+    {
+        "categoria": "Ferramentas Google",
+        "link": "https://cursos.alura.com.br/certificate/d388688b-b8c2-4e3c-99c9-6d454d7ab09d",
+        "nome_curso": "Google Data Studio: acessando o MySQL"
+    },
+    {
+        "categoria": "Ferramentas Google",
+        "link": "https://cursos.alura.com.br/certificate/5f6bdbee-50bd-498c-a48b-0fac52a42ddf",
+        "nome_curso": "Google Data Studio: Parâmetros e Big Query"
+    },
+    {
+        "categoria": "Ferramentas Google",
+        "link": "https://cursos.alura.com.br/certificate/b6dc2eaa-82b4-4c0a-9875-5b7a3ab64f50",
+        "nome_curso": "Google Data Studio: introdução à ferramenta"
+    },
+    {
+        "categoria": "Ferramentas Google",
+        "link": "https://www.udemy.com/certificate/UC-24c9f9e9-de54-4946-a7b5-14746876d207/",
+        "nome_curso": "Domine Google Data Studio: o Curso Completo "
+    },
+    {
+        "categoria": "Ferramentas Google",
+        "link": "https://cursos.alura.com.br/certificate/65c84a42-fbdb-45ba-a97d-7fa3fa3e4bd7",
+        "nome_curso": "UX Design: criando dashboard com Data Studio"
+    },
+    {
+        "categoria": "Ferramentas Google",
+        "link": "https://cursos.alura.com.br/certificate/c7a23324-7b5e-4b40-b43d-e27b4b13fd2a",
+        "nome_curso": "Web Analytics: implemente a ferramenta em seu site"
+    },
+    {
+        "categoria": "Ferramentas Google",
+        "link": "https://cursos.alura.com.br/certificate/c8851a23-bc8b-4f7b-ab11-1886557d2dab",
+        "nome_curso": "Google Analytics parte 2: dimensões e métricas"
+    },
+    {
+        "categoria": "Ferramentas Google",
+        "link": "https://analytics.google.com/analytics/academy/certificate/BY1rvzCLTF-Mbf5RhJ7uWA",
+        "nome_curso": "Google Analytics Advanced"
+    },
+    {
+        "categoria": "Ferramentas Google",
+        "link": "https://analytics.google.com/analytics/academy/certificate/vQkvcww_Swud4eZfuqr9DQ",
+        "nome_curso": "Google Analytics for Beginners"
+    },
+    {
+        "categoria": "Ferramentas Google",
+        "link": "https://www.udemy.com/certificate/UC-ef6d66ab-2b92-4a56-8526-cbbb501d0b9b/",
+        "nome_curso": "Google Analytics COMPLETO - Do Zero ao PRO"
+    },
+    {
+        "categoria": "Ferramentas Google",
+        "link": "https://analytics.google.com/analytics/academy/certificate/fj8SggrYRjCk744h_xYOKg",
+        "nome_curso": "Começar a usar o Google Analytics 360"
+    },
+    {
+        "categoria": "Ferramentas Google",
+        "link": "https://analytics.google.com/analytics/academy/certificate/LXL-etodTXq-3Ma4W81yUw",
+        "nome_curso": "Google Tag Manager Fundamentals"
+    },
+    {
+        "categoria": "Ferramentas Google",
+        "link": "https://www.udemy.com/certificate/UC-aeadaeec-4729-4cec-a447-d0eef86e764f/",
+        "nome_curso": "Google Tag Manager"
+    },
+    {
+        "categoria": "HTMLeCSS",
+        "link": "https://cursos.alura.com.br/certificate/greison-boff/html-css-praticando-html-css",
+        "nome_curso": "HTML e CSS: Praticando html/css"
+    },
+    {
+        "categoria": "HTMLeCSS",
+        "link": "https://cursos.alura.com.br/certificate/7c8184dd-62b8-4585-a0b9-6670ec038369",
+        "nome_curso": "HTML5 e CSS3 parte1: A primeira página da Web"
+    },
+    {
+        "categoria": "HTMLeCSS",
+        "link": "https://cursos.alura.com.br/certificate/6f93199e-38c4-4ac4-8337-3801fbd66039",
+        "nome_curso": "HTML5 e CSS3 parte2: Posicionamento, listas e navegação"
+    },
+    {
+        "categoria": "HTMLeCSS",
+        "link": "https://cursos.alura.com.br/certificate/6c1e9d91-ce30-4bd1-8472-265d30a8058e",
+        "nome_curso": "HTML5 e CSS3 parte3: Trabalhando com formulários e tabelas"
+    },
+    {
+        "categoria": "HTMLeCSS",
+        "link": "https://cursos.alura.com.br/certificate/cb0fb870-9ac8-4c99-98c1-cd8f2900aa01",
+        "nome_curso": "HTML5 e CSS3 parte4: Avançandono CSS"
+    },
+    {
+        "categoria": "JQuery",
+        "link": "https://cursos.alura.com.br/certificate/86d9101b-11cc-4156-b8dd-829d0b80466e",
+        "nome_curso": "JQuery: domine a biblioteca mais popular do mercado parte 1"
+    },
+    {
+        "categoria": "JQuery",
+        "link": "https://cursos.alura.com.br/certificate/9af18d88-0f23-4909-a15c-76f1b0e45d80",
+        "nome_curso": "JQuery: avance na biblioteca mais popular do mercado parte 2"
+    },
+    {
+        "categoria": "JavaScript",
+        "link": "https://cursos.alura.com.br/certificate/greison-boff/javascript-manipulando-dom",
+        "nome_curso": "JavaScript: Manipulando o dom"
+    },
+    {
+        "categoria": "JavaScript",
+        "link": "https://cursos.alura.com.br/certificate/greison-boff/javascript-web-paginas-dinamicas",
+        "nome_curso": "JavaScript para web: Crie páginasdinâmicas"
+    },
+    {
+        "categoria": "JavaScript",
+        "link": "https://cursos.alura.com.br/certificate/greison-boff/javascript-introducao",
+        "nome_curso": "JavaScript: Primeiros passos com alinguagem"
+    },
+    {
+        "categoria": "JavaScript",
+        "link": "https://cursos.alura.com.br/certificate/a1a19746-0023-448b-ae4a-45b1106ab8b6",
+        "nome_curso": "JavaScript: conhecendo o Browser epadrões de projeto"
+    },
+    {
+        "categoria": "JavaScript",
+        "link": "https://cursos.alura.com.br/certificate/1fd4ca85-8519-4995-892b-433b7edba55d",
+        "nome_curso": "JavaScript: programando a Orientação aObjetos"
+    },
+    {
+        "categoria": "JavaScript",
+        "link": "https://cursos.alura.com.br/certificate/550f31cd-8764-40eb-8ac0-fd18eeb6ab7d",
+        "nome_curso": "JavaScript: programando na linguagem daweb"
+    },
+    {
+        "categoria": "JavaScript",
+        "link": "https://cursos.alura.com.br/certificate/02587b60-edfd-402f-acd9-be35484ff67d",
+        "nome_curso": "JavaScript na Web: Manipule o DOM comJavaScript"
+    },
+    {
+        "categoria": "JavaScript",
+        "link": "https://cursos.alura.com.br/certificate/9d63e194-b684-43ad-8ea0-804cc73de40b",
+        "nome_curso": "JavaScript na Web: Armazenando dados nonavegador"
+    },
+    {
+        "categoria": "JavaScript",
+        "link": "https://gyncursos.com.br/wp-content/uploads/2022/03/11824_422426.pdf",
+        "nome_curso": "JavaScript"
+    },
+    {
+        "categoria": "Certifições complementares",
+        "link": "https://cursos.alura.com.br/certificate/greison-boff/worpress-sites-elementor",
+        "nome_curso": "Wordpress: sites com Elementor"
+    },
+    {
+        "categoria": "Certifições complementares",
+        "link": "https://cursos.alura.com.br/certificate/b73c64cf-9d4f-4e19-bee5-d5237becb281",
+        "nome_curso": "Expressões regulares: capturando textos de forma mágica"
+    },
+    {
+        "categoria": "Certifições complementares",
+        "link": "https://cursos.alura.com.br/certificate/aeede2db-6330-4087-ae30-b810f4d24bed",
+        "nome_curso": "HTTP: Entendendo a web por baixo dos panos"
+    }
+]
+
+
+
+createDivUl = (e) => {
+    elementDivUl = document.createElement('div');
+    elementDivUl.setAttribute('class', e.replace(' ', '_'));
+    elementDivUl.innerHTML = `<h3>${e}</h3>
+                                <ul data-category= ${e.replace(' ', '_')}></ul>
+                            `
+    document.querySelector('body .certificates').append(elementDivUl);
+}
+
+createLi = (categoria, link, nome) => {
+    elemetLi = document.createElement('li');
+    elemetLi.innerHTML = `<a href=${link} data-category = ${categoria.replace(' ', '_')} target="_blank">${nome}</a>`;
+    document.querySelector(`body .certificates div.${categoria.replace(' ', '_')}`).append(elemetLi);
+}
+
+var returnElement = (e) => {
+    for (a of e) {
+        if (document.querySelectorAll(`body div.${a.categoria.replace(' ', '_')}`).length === 0) {
+            createDivUl(a.categoria);
+        }
+        createLi(a.categoria, a.link, a.nome_curso);
+    }
+}
+returnElement(jsonInfos);
+/*
+fetch('http://127.0.0.1:5500/rot.json')
+    .then((response) => response.json())
+    .then((json) => returnElement(json));
+*/
+showElement = (e) => {
+    for (let i = 0; i < document.querySelectorAll('#boxMenu li').length; i++) {
+        document.querySelectorAll('#boxMenu li')[i].classList.remove('active');
+    }
+    e.classList.add('active');
+    for (let i = 0; i < document.querySelectorAll('.boxElements').length; i++) {
+        if (!document.querySelectorAll('.boxElements')[i].classList.value.includes('hide')) {
+            document.querySelectorAll('.boxElements')[i].classList.add('hide');
+        }
+    }
+    document.querySelector(`.${e.getAttribute('id')}`).classList.remove('hide');
+}  
+
+showElementProject = (e) => {
+    for (let i = 0; i < document.querySelectorAll('.projectsList').length; i++) {
+        document.querySelectorAll('.projectsList')[i].classList.remove('projectsListActive');
+    }
+    for (let i = 0; i < document.querySelectorAll('.projectsListBox').length; i++) {
+        if (!document.querySelectorAll('.projectsListBox')[i].classList.value.includes('hideProjects')) {
+            document.querySelectorAll('.projectsListBox')[i].classList.add('hideProjects');
+        }
+    }
+    e.classList.add('projectsListActive');
+    document.querySelector(`.projectsBox #${e.getAttribute('id')}`).classList.remove('hideProjects');
+}  
+
+device = () => {
+    const ua = navigator.userAgent;
+    if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+        return "tablet";
+    }
+    if (/Mobile|iP(hone|od)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
+        return "mobile";
+    }
+    return "desktop";
+};
+if(device() != "desktop"){
+    document.querySelector('body').classList.add('mobile');
+}
